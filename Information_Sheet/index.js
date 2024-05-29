79ecde96664b719fc3413e53e9df321ab6686475
AlwaysByrnes <141357497+AlwaysByrnes@users.noreply.github.com>
2024-05-29 13:28:11 -0400

    changes

--- Information_Sheet/index.js
+++ Information_Sheet/index.js
@@ -6,28 +6,6 @@ let emailText = document.getElementById('emailTxt')
 let submitBtn = document.getElementById('submitBtn')
 let ul = document.getElementById('info')
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    //Obtain saved info from local storage
-    const saveName = localStorage.getItem('name')
-    const saveLastName = localStorage.getItem('lastName')
-    const savePhoneNumber = localStorage.getItem('phoneNumber')
-    const saveEmail = localStorage.getItem('saveEmail')
-    //Check for falsey values
-    if(saveName || saveLastName || savePhoneNumber || saveEmail){
-        let li = document.createElement('li')
-        //Print out list to the page
-            li.textContent = 
-            `First Name: ${name} 
-            Last Name: ${lastName}
-            Phone Number: ${phoneNumber}
-            Email: ${email}`
-            ul.appendChild(li)
-    }
-
-
-})
-
-
 submitBtn.addEventListener("click", ()=>{
     //Create a li element
     
@@ -45,16 +23,21 @@ submitBtn.addEventListener("click", ()=>{
     
      // Create a list item to display the user information
      let li = document.createElement('li')
-     li.textContent = 
-     `First Name: ${name} 
+     
+      li.textContent += 
+     `First Name:${name} 
      Last Name: ${lastName}
      Phone Number: ${phoneNumber}
      Email: ${email}`
-     ul.appendChild(li)
-
+    
+     ul.appendChild(li) 
+     
+    
+     
     //Save to local storage
     localStorage.setItem('First Name', name)
     localStorage.setItem('Last Name', lastName)
     localStorage.setItem('Phone Number', phoneNumber)
     localStorage.setItem('Email', email)
+    
 })
