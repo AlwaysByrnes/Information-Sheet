2e50b0bf78f8bd90b83ffa9f0eb739b73e15f42a
AlwaysByrnes <141357497+AlwaysByrnes@users.noreply.github.com>
2024-06-01 17:22:47 -0400

    Added format phone number function

--- Information_Sheet/index.js
+++ Information_Sheet/index.js
@@ -6,8 +6,19 @@ let emailText = document.getElementById('emailTxt')
 let submitBtn = document.getElementById('submitBtn')
 let ul = document.getElementById('info')
 
+
 submitBtn.addEventListener("click", ()=>{
-    //Create a li element
+//Function to make the phone number look like a phone number
+  function formatPhoneNumber(phoneNumber) {
+  let cleaned = ('' + phoneNumber).replace(/\D/g, '');
+  let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+  }
+  
+  return phoneNumber;
+}
+
     
     //Set new variable names to the text values (Makes it look less confuesing)
     const name = userNameTxt.value
@@ -21,19 +32,21 @@ submitBtn.addEventListener("click", ()=>{
         return
     }
     
+    //Funtion to print out the formatted phone number
+    const formattedPhoneNumber = formatPhoneNumber(phoneNumber)
+
      // Create a list item to display the user information
      let li = document.createElement('li')
      
-      li.textContent += 
+      li.textContent = 
      `First Name:${name} 
      Last Name: ${lastName}
-     Phone Number: ${phoneNumber}
+     Phone Number: ${formattedPhoneNumber}
      Email: ${email}`
-    
+     
      ul.appendChild(li) 
      
     
-     
     //Save to local storage
     localStorage.setItem('First Name', name)
     localStorage.setItem('Last Name', lastName)
